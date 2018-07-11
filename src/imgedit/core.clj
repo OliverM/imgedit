08caabaca9c21f0b67a36fdbfba6aa6828ca4549
Oliver Mooney <oliver.mooney@gmail.com>
2018-07-11 22:55:01 +0100

    Initial parser
    - untested

--- src/imgedit/core.clj
+++ src/imgedit/core.clj
@@ -1,5 +1,32 @@
 (ns imgedit.core
+  (:require [instaparse.core :as insta])
   (:gen-class))
+
+(def command-parser
+  (insta/parser "
+     S = COMMAND
+     COMMAND = NEW | CLEAR | PIXEL | VERTICAL | HORIZONTAL | FILL | SHOW | QUIT
+     NEW = 'I' <ws> WIDTH <ws> HEIGHT
+     CLEAR = 'C' <ws> XY
+     PIXEL = 'L' <ws> XYC
+     SEGMENT = DIRECTION <ws> X <ws> Y <ws> Y2 <ws> COLOUR
+     DIRECTION = VERTICAL | HORIZONTAL
+     VERTICAL = 'V'
+     HORIZONTAL = 'H'
+     FILL = 'F' <ws> XYC
+     SHOW = 'S'
+     QUIT = 'X'
+     XYC = XY <ws> COLOUR
+     XY = X <ws> Y
+     COLOUR = #'[A-Z]'
+     X = number
+     Y = number
+     Y2 = number
+     WIDTH = number
+     HEIGHT = number
+     number = #'[0-9]+'
+     ws = #'\\s+'
+     "))
 
 (defn -main
   "I don't do a whole lot ... yet."
