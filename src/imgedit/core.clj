3ffa1faf2e3c84a0138154442e9cbf934d99c4ae
Oliver Mooney <oliver.mooney@gmail.com>
2018-07-12 11:55:21 +0100

    Added initial spec for commands
    - also added instaparse transformer to read numerical arguments as numbers

--- src/imgedit/core.clj
+++ src/imgedit/core.clj
@@ -4,11 +4,12 @@
   (:gen-class))
 
 (def command-parser
+  "Generate a parser for image editor commands."
   (insta/parser "
      S = COMMAND
      <COMMAND> = NEW | CLEAR | PIXEL | VERTICAL | HORIZONTAL | FILL | SHOW | QUIT
      NEW = <'I'> <ws> WIDTH <ws> HEIGHT
-     CLEAR = <'C'> <ws> XY
+     CLEAR = <'C'>
      PIXEL = <'L'> <ws> XYC
      VERTICAL = <'V'> <ws> SEGMENT
      HORIZONTAL = <'H'> <ws> SEGMENT
@@ -29,6 +30,50 @@
      <coord> = #'[1-9][0-9]*'
      ws = #'\\s+'
      "))
+
+(defn param-transform
+  "Given a parse-tree from command-parser, above, convert the :coord terminals to
+  numbers."
+  [parsed-command]
+  (letfn [(reader [tag v] (vector tag (clojure.edn/read-string v)))]
+    (insta/transform
+      {:X #(reader :X %)
+       :Y #(reader :Y %)
+       :U #(reader :U %)
+       :V1 #(reader :V1 %)
+       :V2 #(reader :V2 %)
+       :WIDTH #(reader :WIDTH %)
+       :HEIGHT #(reader :HEIGHT %)}
+      parsed-command)))
+
+(def XY-MAX 250)
+(s/def ::coord-value (s/and int? #(< 1 % XY-MAX)))
+(s/def ::coord-name #{:X :Y :U :V1 :V2})
+(s/def ::coord (s/tuple ::coord-name ::coord-value))
+(s/def ::dimension-name #{:WIDTH :HEIGHT})
+(s/def ::dimension (s/tuple ::dimension-name ::coord-value))
+(s/def ::colour-value (s/and string? #(re-matches #"[A-Z]" %)))
+(s/def ::colour (s/tuple #{:COLOUR} ::colour-value))
+(s/def ::param (s/or :dimension ::dimension :coord ::coord :colour ::colour))
+
+(s/def ::new-image (s/tuple #{:NEW} ::dimension ::dimension))
+(s/def ::clear-image (s/tuple #{:CLEAR}))
+(s/def ::pixel (s/tuple #{:PIXEL} ::coord ::coord ::colour))
+(s/def ::vertical (s/tuple #{:VERTICAL} ::coord ::coord ::coord ::colour))
+(s/def ::horizontal (s/tuple #{:HORIZONTAL} ::coord ::coord ::coord ::colour))
+(s/def ::fill (s/tuple #{:FILL} ::coord ::coord ::colour))
+(s/def ::show (s/tuple #{:SHOW}))
+(s/def ::quit (s/tuple #{:QUIT}))
+
+(s/def ::command (s/or
+                   :new ::new-image
+                   :clear ::clear-image
+                   :pixel ::pixel
+                   :vertical ::vertical
+                   :horizontal ::horizontal
+                   :fill ::fill
+                   :show ::show
+                   :quit ::quit))
 
 (defn -main
   "I don't do a whole lot ... yet."
